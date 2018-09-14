ddb430f14db26c0994c92628aed5617c42194f1a
JongGyun Park <jonggyuni@gmail.com>
2018-09-14 21:41:59 +0900

    back-end default re-setting

--- movie-backend/src/api/movies/movies.ctrl.js
+++ movie-backend/src/api/movies/movies.ctrl.js
@@ -13,27 +13,82 @@ const movies = [
     }
 ];
 
-exports.list = async (ctx) => {
-    console.log('im here.....');
-
+// 1. 일별 박스 오피스
+exports.dailyList = async (ctx) => {
     request({
-        url: url,
+        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101",
         json: true
     }, (error, response, body) => {
-        console.log('in the request');
-        console.log(body.boxOfficeResult);
-    } );
-
+        console.log(body);
+    });
 }
 
-exports.detail = async (ctx) => {
-    console.log("im not here");
+// 2. 주간/주말 박스 오피스
+exports.weeklyList = async (ctx) => {
     request({
-        url: url,
+        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json?key=430156241533f1d058c603178cc3ca0e&targetDt=20120101",
         json: true
     }, (error, response, body) => {
-        console.log('in the request2');
-        console.log(body.boxOfficeResult);
-    } );
+        console.log(body);
+    });
+}
 
-} 
+// 3. 영화목록
+exports.movieList = async (ctx) => {
+    request({
+        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=430156241533f1d058c603178cc3ca0e",
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
+
+// 4. 영화상세정보
+exports.movieInfoList = async (ctx) => {
+    request({
+        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=430156241533f1d058c603178cc3ca0e&movieCd=20124079",
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
+
+// 5. 영화사정보
+exports.companyList = async (ctx) => {
+    request({
+        url: "http://kobis.or.kr/kobisopenapi/webservice/rest/company/searchCompanyList.json?key=430156241533f1d058c603178cc3ca0e",
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
+
+// 6. 영화사 상세정보
+exports.companyInfoList = async (ctx) => {
+    request({
+        url: "http://kobis.or.kr/kobisopenapi/webservice/rest/company/searchCompanyInfo.json?key=430156241533f1d058c603178cc3ca0e&companyCd=20122497",
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
+
+// 7. 영화인 정보
+exports.peopleList = async (ctx) => {
+    request({
+        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=430156241533f1d058c603178cc3ca0e",
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
+
+// 8. 영화인 상세정보
+exports.peopleInfoList = async (ctx) => {
+    request({
+        url: "http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleInfo.json?key=430156241533f1d058c603178cc3ca0e&peopleCd=20164556",
+        json: true
+    }, (error, response, body) => {
+        console.log(body);
+    });
+}
